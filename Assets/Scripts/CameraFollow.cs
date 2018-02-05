c9d55a345cf227d380be4a0fc4d7d57b7bb84172
Orri Axelsson <orriaxels@gmail.com>
2018-02-05 15:00:55 +0000

    cameras added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	public Vector3 offset;
+	
+	void LateUpdate () 
+	{
+		transform.position = new Vector3(target.position.x + offset.x, offset.y, offset.z);
+	}
+
+}
+
+
---
+++ Assets/Scripts/CharacterCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{     
+		if(col.gameObject.name == "Hovering_platform")
+		{
+			transform.parent = col.transform;
+		}
+	}
+ 
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if(col.gameObject.name == "Hovering_platform")
+		{
+			transform.parent = null; 
+		}
+	} 
+}
