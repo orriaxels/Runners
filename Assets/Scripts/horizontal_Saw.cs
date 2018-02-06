99fa6ab50d6068b3c36af1bda2ec89e228b13c82
haraldurshoshan <haraldurshoshan@gmail.com>
2018-02-06 13:57:44 +0000

    saw scripts and prefab added

---
+++ Assets/Scripts/horizontal_Saw.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class horizontal_Saw : MonoBehaviour {
+	public bool left;
+	public float rotationSpeed = 180;
+	
+	void Start () 
+	{	
+	}
+	
+	void Update () 
+	{
+		Vector3 sawPos = transform.position;
+		Quaternion sawRotation = transform.rotation;
+		float z = sawRotation.eulerAngles.z;
+		z -= rotationSpeed * Time.deltaTime;
+		sawRotation = Quaternion.Euler(0, 0, z);
+
+		if(left)
+		{
+			sawPos.x -= Time.deltaTime;
+		}
+		else
+		{
+			sawPos.x += Time.deltaTime;
+		}
+
+		transform.position = sawPos;
+		transform.rotation = sawRotation;
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.tag == "sawLeft")
+		{
+			left = false;
+		}
+		else if(col.tag == "sawRight")
+		{
+			left = true;
+		}
+	}
+}
---
+++ Assets/Scripts/vertical_Saw.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vertical_Saw : MonoBehaviour {
+	
+	public bool up;
+	public float rotationSpeed = 180;
+	
+	void Start () {
+		
+	}
+	
+	void Update () 
+	{
+		Vector3 sawPos = transform.position;
+		Quaternion sawRotation = transform.rotation;
+		float z = sawRotation.eulerAngles.z;
+		z -= rotationSpeed * Time.deltaTime;
+		sawRotation = Quaternion.Euler(0, 0, z);
+
+		if(up)
+		{
+			sawPos.y += Time.deltaTime;
+		}
+		else
+		{
+			sawPos.y -= Time.deltaTime;
+		}
+
+		transform.position = sawPos;
+		transform.rotation = sawRotation;
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.tag == "sawUp")
+		{
+			up = false;
+		}
+		else if(col.tag == "sawDown")
+		{
+			up = true;
+		}
+	}
+}
