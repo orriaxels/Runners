cf5520a1a06590791fe7efb0595bf77c48c42e2e
haraldurshoshan <haraldurshoshan@gmail.com>
2018-02-07 14:24:52 +0000

    player scripts

--- Assets/Scripts/LowerPlayerUserControl.cs
+++ Assets/Scripts/LowerPlayerUserControl.cs
@@ -40,7 +40,7 @@ namespace UnityStandardAssets._2D
 
         void OnTriggerEnter2D(Collider2D other)
         {
-            if(other.tag == "dropZone")
+            if(other.tag == "dropZone" || other.gameObject.name == "Saw")
             {
                 transform.position = respawnPoint;
             }
